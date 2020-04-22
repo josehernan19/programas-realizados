4bacf3047d09054da0b75f4349851d10c94eaa26
Jose Hernandez <antonioaljorna@gmail.com>
2020-04-22 17:09:06 -0300

    programa de calculo de peso de payasos y muneca

---
+++ calculo_de_peso_total.py
@@ -0,0 +1,6 @@
+muneca=int(input('cuantos munecas enviaras? '))
+payasos=int(input('y cuantos payasos? '))
+peso_munecas= 75 * muneca
+peso_payaso= 112 * payasos
+peso_total= peso_munecas + peso_payaso
+print('el peso total de tu paquete es ', peso_total, 'gramos')
